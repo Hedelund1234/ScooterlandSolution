b9149a52def91a08f60c84ad38357640aa3e31b5
Frederik Hedelund Pedersen <frederikhedelund@gmail.com>
2024-11-19 11:37:13 +0100

    DbContext added ConnectionHandler Added

---
+++ Scooterland/Server/DataAccess/ConnectionHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace Scooterland.Server.DataAccess
+{
+	public class ConnectionHandler
+	{
+		//public string GetConnectionStringSQLClient()
+		//{
+		//	return ConfigurationManager.ConnectionStrings["Frederik"].ToString();
+		//}
+		public static string GetConnectionStringEF()
+		{
+			return "Server=FREDERIK;Database=ScooterlandDb;Trusted_Connection=True;TrustServerCertificate=True;";
+		}
+	}
+}
--- Scooterland/Server/DataAccess/ScooterlandDbContext.cs
+++ Scooterland/Server/DataAccess/ScooterlandDbContext.cs
@@ -1,6 +1,24 @@
-﻿namespace Scooterland.Server.DataAccess
+﻿using Microsoft.EntityFrameworkCore;
+using Scooterland.Shared.Models;
+
+namespace Scooterland.Server.DataAccess
 {
-	public class ScooterlandDbContext
+	public class ScooterlandDbContext:DbContext
 	{
+		public DbSet<Customer> Customers { get; set; }
+		public DbSet<Employee> Employees { get; set; }
+		public DbSet<Product> Products { get; set; }
+		public DbSet<Sale> Sales { get; set; }
+		public DbSet<SalesLineItem> SalesLineItems { get; set; }
+		public DbSet<Specialization> Specializations { get; set; }
+
+		
+		public string DbPath { get; }
+
+		// The following configures EF to create a Sqlite database file in the
+		// special "local" folder for your platform.
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+			=> optionsBuilder.UseSqlServer(ConnectionHandler.GetConnectionStringEF());
 	}
+}
 }
