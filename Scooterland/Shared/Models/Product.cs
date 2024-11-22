6c55965811a9520a040309eb8372da9a5a9436b0
Frederik Hedelund Pedersen <frederikhedelund@gmail.com>
2024-11-22 13:13:58 +0100

    Opdateret ProductCreate Form samt validerings errormessage på Product model

--- Scooterland/Shared/Models/Product.cs
+++ Scooterland/Shared/Models/Product.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +12,12 @@ namespace Scooterland.Shared.Models
 	{
 		public int ProductId { get; set; }
 		[MaxLength(70)]
+		[StringLength(70, ErrorMessage ="Navnet må maksimalt være 70 anslag lang!")]
 		public string Name { get; set; }
 		[MaxLength(30)]
+		[StringLength(30, ErrorMessage ="Type må maksimalt være 30 anslag lang!")]
 		public string Type { get; set; }
+		[Range(minimum:0,maximum:System.Int32.MaxValue, ErrorMessage = "Prisen skal være positiv!")]
 		public decimal Price { get; set; }
 		public List<SalesLineItem> SalesLineItems { get; set; } = new List<SalesLineItem>();
 
