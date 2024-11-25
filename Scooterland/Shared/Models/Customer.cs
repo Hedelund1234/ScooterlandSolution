6079619fb92c13d471642aa45935127bdc850bc9
Frederik Hedelund Pedersen <frederikhedelund@gmail.com>
2024-11-25 18:54:18 +0100

    Create Customer form

--- Scooterland/Shared/Models/Customer.cs
+++ Scooterland/Shared/Models/Customer.cs
@@ -5,13 +5,13 @@ namespace Scooterland.Shared.Models
 	public class Customer
 	{
 		public int CustomerId { get; set; }
-		[MaxLength(70)]
+		[MaxLength(70, ErrorMessage = "Navnet må maksimalt være 70 tegn")]
 		public string Name { get; set; }
-		[MaxLength(100)]
+		[MaxLength(100, ErrorMessage = "Email må maksimalt være 100 tegn")]
 		public string Email { get; set; }
-		[MaxLength(20)]
+		[MaxLength(20, ErrorMessage = "Telefon nummer må maksimalt være 20 tegn lang")]
 		public string Phonenumber { get; set; }
-		[MaxLength(150)]
+		[MaxLength(150, ErrorMessage = "Adressen må maksimalt være 150 tegn lang")]
 		public string Address { get; set; }
 		public List<Sale> Sales { get; set; } = new List<Sale>();
 
