e367c88028f16de5f95a23212bd681ee57a4e33b
NKB89 <nikolasbech@gmail.com>
2024-11-26 10:59:00 +0100

    Added EmployeeService og IEmploteeService

--- Scooterland/Client/Services/EmployeeServices/IEmployeeService.cs
+++ Scooterland/Client/Services/EmployeeServices/IEmployeeService.cs
@@ -1,6 +1,17 @@
-﻿namespace Scooterland.Client.Services.EmployeeServices
+﻿using Scooterland.Shared.Models;
+
+namespace Scooterland.Client.Services.EmployeeServices
 {
 	public interface IEmployeeService
 	{
-	}
+        Task<Employee[]?> GetAllEmployees();
+
+        Task<Employee?> GetEmployee(int id);
+
+        Task<int> AddEmployee(Employee employee);
+
+        Task<int> DeleteEmployee(int id);
+
+        Task<int> UpdateEmployee(Employee employee);
+    }
 }
