f5df7265835fd3c66caf77d1c569012e54840cf6
NKB89 <nikolasbech@gmail.com>
2024-11-26 10:40:11 +0100

    Addet metoder til IEmployeeRepository.cs

--- Scooterland/Server/Repositories/EmployeeRepository/IEmployeeRepository.cs
+++ Scooterland/Server/Repositories/EmployeeRepository/IEmployeeRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace Scooterland.Server.Repositories.EmployeeRepository
+﻿using Scooterland.Shared.Models;
+
+namespace Scooterland.Server.Repositories.EmployeeRepository
 {
 	public interface IEmployeeRepository
 	{
+		List<Employee> GetAllEmployees();
+		Employee FindEmployee(int id);
+		bool AddEmployee(Employee employee);
+		bool DeleteEmployee(int id);
+		bool UpdateEmployee(Employee employee);
 	}
 }
