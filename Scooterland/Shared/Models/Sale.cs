b269d1229b5e4ee30763c08b2f9136b331c585ac
Hjaltechristensen <hjalte-christensen@live.dk>
2024-11-27 12:23:37 +0100

    Sale kan oprettes i database med customerId

--- Scooterland/Shared/Models/Sale.cs
+++ Scooterland/Shared/Models/Sale.cs
@@ -10,8 +10,8 @@ namespace Scooterland.Shared.Models
 	public class Sale
 	{
 		public int? SaleId { get; set; }
-		public int CustomerId { get; set; }
-		public Customer Customer { get; set; }
+		public int? CustomerId { get; set; }
+		public Customer? Customer { get; set; }
 		public int? EmployeeId { get; set; }
 		public Employee? Employee { get; set; }
 		public DateTime? StartDate { get; set; }
