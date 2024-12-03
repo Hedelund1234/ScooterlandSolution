35c22099a8acb9e064a770ee87349dd96efa4013
Frederik Pedersen <frederikhedelund@gmail.com>
2024-12-03 20:37:37 +0100

    Added ability to assign specialization to employee and remove

--- Scooterland/Server/Repositories/EmployeeRepository/EmployeeRepositoryEF.cs
+++ Scooterland/Server/Repositories/EmployeeRepository/EmployeeRepositoryEF.cs
@@ -47,18 +47,39 @@ namespace Scooterland.Server.Repositories.EmployeeRepository
 		public bool UpdateEmployee(Employee employee)
 		{
 			var db = new ScooterlandDbContext();
-			Employee foundEmployee = db.Employees.Where(x => x.EmployeeId == employee.EmployeeId).FirstOrDefault();
+			Employee foundEmployee = db.Employees.Where(x => x.EmployeeId == employee.EmployeeId).Include(s => s.Specializations).FirstOrDefault();
+			
 
 			var originalEmployee = foundEmployee;
+			//Remvoes specialization from EmployeeSpecialization table
+			if (employee.Specializations.Count() < foundEmployee.Specializations.Count())
+			{
+				var specializationsToRemove = foundEmployee.Specializations 
+							.Where(f => !employee.Specializations // Filter foundEmployee.Specializations to find those not in employee.Specializations
+                            .Any(employee => employee.SpecializationId == f.SpecializationId)) // Check if any Specialization in employee.Specializations has the same ID as foundEmployee.Specializations
+                            .ToList();
 
-			foundEmployee.Name = employee.Name;
+				foreach (var specialization in specializationsToRemove)
+				{
+					foundEmployee.Specializations.Remove(specialization);
+				}
+			}
+
+			//Adds specialization from EmployeeSpecialization table
+            if (employee.Specializations.Count() > foundEmployee.Specializations.Count())
+            {
+				if (!foundEmployee.Specializations.Exists(s => s.SpecializationId == employee.Specializations.Last().SpecializationId)) //Extra validation, checks new specialization already exist/is assigned on employee from database
+                {
+                    foundEmployee.Specializations.Add(employee.Specializations.Last());
+                }
+            }
+
+            foundEmployee.Name = employee.Name;
 			foundEmployee.Role = employee.Role;
-			//foundEmployee.Specializations = employee.Specializations;
 			db.SaveChanges();
 
 			if (originalEmployee.Name != foundEmployee.Name ||
-				originalEmployee.Role != foundEmployee.Role /*||
-				originalEmployee.Specializations != foundEmployee.Specializations*/)
+				originalEmployee.Role != foundEmployee.Role)
 			{
 				return true;
 			}
@@ -92,9 +113,7 @@ namespace Scooterland.Server.Repositories.EmployeeRepository
 			List<Employee> employees;
 			try
 			{
-				employees = db.Employees.ToList();
-
-				//employees = db.Employees.Include(s => s.Specializations).ToList();
+				employees = db.Employees.Include(s => s.Specializations).ToList();
 			}
 			catch
 			{
