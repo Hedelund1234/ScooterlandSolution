30c13ca2d3f22c23f525fdd66b1e22a7b6163832
Frederik Hedelund Pedersen <frederikhedelund@gmail.com>
2024-12-09 11:48:16 +0100

    NUnitTest on Validation and Repositories (Customer & Employee)

--- NUnitTest/UnitTest1.cs
+++ NUnitTest/UnitTest1.cs
@@ -1,16 +1,63 @@
+using Scooterland.Server.Repositories.CustomerRepository;
+using Scooterland.Server.Repositories.EmployeeRepository;
+using Scooterland.Server.Validators;
+using Scooterland.Shared.Models;
+
 namespace NUnitTest
 {
     public class Tests
     {
-        [SetUp]
+		MyValidator validator;
+		[SetUp]
         public void Setup()
         {
-        }
+			validator = new MyValidator();
+		}
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
-    }
+		// Customer Test
+		[Test]
+		public void CreateCustomerValidationTest()
+		{
+            var mockCustomer = new Customer { CustomerId = 0, 
+                                              Name = "Jens Christian",  
+                                              Email = "Jens@mail.dk",
+                                              Phonenumber = "12345789", 
+                                              Address = "Kolding vej 2 - 6000 Kolding"
+											};
+            Assert.That(validator.CustomerCreateValidation(mockCustomer), Is.True);
+		}
+
+		[Test]
+		public async Task CustomerGetAllRepoTest()
+		{
+            CustomerRepositoryEF repo = new CustomerRepositoryEF();
+			var customers = repo.GetAllCustomers();
+            Assert.That(customers, Is.Not.Empty);
+		}
+
+
+
+
+		// Employee Test
+		[Test]
+		public void CreateEmployeeValidationTest()
+		{
+			var mockEmployee = new Employee
+			{
+				EmployeeId = 0,
+				Name = "Lars Landevej",
+				Role = "Mekaniker",
+			};
+			Assert.That(validator.EmployeeCreateValidation(mockEmployee), Is.True);
+		}
+
+
+		[Test]
+		public async Task EmployeeGetAllRepoTest()
+		{
+			EmployeeRepositoryEF repo = new EmployeeRepositoryEF();
+			var employee = repo.GetAllEmployees();
+			Assert.That(employee, Is.Not.Empty);
+		}
+	}
 }
